ba32d2d7c36e4718845ae4a338b29c19e6c42271
Adipramana <herryprasetyo.dev@gmail.com>
2025-08-16 17:25:30 +0700

    update

--- add.php
+++ add.php
@@ -1,17 +1,24 @@
-<?php 
+<?php
 include "conn.php"; // Include the connection file
 
+// Check if request method is POST
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    echo json_encode(['error' => 'Only POST method is allowed']);
+    exit();
+}
+
 // Assign values from POST request
-$item_code =$_POST['item_code'];
-$item_name = $_POST['item_name'];
-$price = $_POST['price'];
-$stock = $_POST['stock'];
+$item_code = isset($_POST['item_code']) ? trim($_POST['item_code']) : '';
+$item_name = isset($_POST['item_name']) ? trim($_POST['item_name']) : '';
+$price = isset($_POST['price']) ? filter_var($_POST['price'], FILTER_VALIDATE_FLOAT) : 0;
+$stock = isset($_POST['stock']) ? filter_var($_POST['stock'], FILTER_VALIDATE_INT) : 0;
+
 
 // Function to save data in the database
 $conn->query("INSERT INTO tb_item(item_code, item_name, price, stock)
             VALUES(
-                '".$item_code."',  
-                '".$item_name."',
-                '".$price."',
-                '".$stock."')");
+                '" . $item_code . "',  
+                '" . $item_name . "',
+                '" . $price . "',
+                '" . $stock . "')");
 ?>
--- conn.php
+++ conn.php
@@ -5,7 +5,7 @@ $password = "";
 $dbname = "db_inventori";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli(hostname: $servername, username: $username, password: $password, database: $dbname);
 
 // Check connection
 if (!$conn->connect_error) {
